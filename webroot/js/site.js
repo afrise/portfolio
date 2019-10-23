e4c410724a5b85c2d320ca34362f4ef8bba202ad
Allen Frise <afrise@gmail.com>
2019-10-22 21:04:06 -0400

    ui bugfixes, jspdf support

--- webroot/js/site.js
+++ webroot/js/site.js
@@ -6,3 +6,11 @@ $("body").on("click", "[data-trigger-button]", function(){if (window.matchMedia(
 $.getJSON('./resume.json',   function(data){var resume   = new Vue({el: '#resume-data', data: data});})
 $.getJSON('./projects.json', function(data){var projects = new Vue({el: '#projects',    data: data});})
 $.getJSON('./about.json',    function(data){var projects = new Vue({el: '#about-data',  data: data});})
+
+function GetPdf(){
+    var pdf = new jsPDF("portrait", "mm", "letter");
+    pdf.setFontSize(4);
+    pdf.fromHTML(document.getElementById("resume-data").innerHTML, 25, 15, {width: 160});
+    pdf.setTextColor(0,0,0);
+    pdf.save('resume.pdf');
+}
