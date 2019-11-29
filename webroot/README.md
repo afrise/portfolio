bd1829309848885292d9973e05c7bf9358948870
Allen Frise <afrise@gmail.com>
2019-11-29 14:25:12 -0500

    update with changes fron github

---
+++ webroot/README.md
@@ -0,0 +1,3 @@
+# afrise.github.io
+My portfolio. Build with vue.js and bootstrap, with data pulled from static JSON files allowing for a future backend implementation
+
--- webroot/js/site.js
+++ webroot/js/site.js
@@ -3,14 +3,15 @@ $("body").on("click", "[data-trigger-button]", function(){if (window.matchMedia(
     var $this = $(this), cNavButton = $this.data("trigger-button");$(cNavButton).trigger('click');}});
 
 //load up JSON data and create Vue objects
-$.getJSON('./resume.json',   function(data){var resume   = new Vue({el: '#resume-data', data: data});})
-$.getJSON('./projects.json', function(data){var projects = new Vue({el: '#projects',    data: data});})
-$.getJSON('./about.json',    function(data){var projects = new Vue({el: '#about-data',  data: data});})
+$.getJSON('./json/resume.json',   function(data){var resume   = new Vue({el: '#resume-data', data: data});})
+$.getJSON('./json/projects.json', function(data){var projects = new Vue({el: '#projects',    data: data});})
+$.getJSON('./json/about.json',    function(data){var about    = new Vue({el: '#about-data',  data: data});})
+$.getJSON('./json/contact.json',  function(data){var contact  = new Vue({el: '#contacts',    data: data});})
 
 function GetPdf(){
     var pdf = new jsPDF("portrait", "mm", "letter");
     pdf.setFontSize(4);
-    pdf.fromHTML(document.getElementById("resume-data").innerHTML, 20, 10, {width: 180});
+    pdf.fromHTML(document.getElementById("resume-data").innerHTML.replace(/�/g, ">"), 20, 10, {width: 180});
     pdf.setTextColor(0,0,0);
     pdf.save('resume.pdf');
 }
